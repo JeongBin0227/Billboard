35460948efb7c3581e75de367d6e632d39251cd8
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-10-18 23:48:01 +0900

    feat: 비동기 예시

--- billboard/src/recoil/count.js
+++ billboard/src/recoil/count.js
@@ -1,4 +1,24 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
+
+const getName = (name) =>
+  new Promise((resolve) => {
+    window.setTimeout(() => {
+      resolve({ name });
+    }, 1000);
+  });
+
+export const currentUserNameState = atom({
+  key: 'currentUserNameState',
+  default: 'Kkangil',
+});
+
+export const currentUserName = selector({
+  key: 'currentUserName',
+  get: async ({ get }) => {
+    const response = await getName(get(currentUserNameState));
+    return response.name;
+  },
+});
 
 export const countState = atom({
   key: 'countState',

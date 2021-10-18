143ca65913be17f0555aafd34294ec79968fdb4f
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-10-18 23:43:13 +0900

    feat: selector setting

--- billboard/src/components/Count/Count.js
+++ billboard/src/components/Count/Count.js
@@ -4,13 +4,19 @@ import { countState } from '../../recoil/count';
 
 function Count() {
   const [count, setCount] = useRecoilState(countState);
+  const [evenCount, setEvenCount] = useRecoilState(countEvenState);
   const incrementByOne = () => setCount(count + 1);
+  const incrementByOneEvenCount = () => setEvenCount(count + 1);
 
   return (
     <div>
       Count: {count}
       <br />
       <button onClick={incrementByOne}>Increment</button>
+      <br />
+      Even Count: {evenCount ? '짝수' : '홀수'}
+      <br />
+      <button onClick={incrementByOneEvenCount}>Even Increment</button>
     </div>
   );
 }
--- billboard/src/recoil/count.js
+++ billboard/src/recoil/count.js
@@ -4,3 +4,9 @@ export const countState = atom({
   key: 'countState',
   default: 0,
 });
+
+export const countEvenState = selector({
+  key: 'countEvenState',
+  get: ({ get }) => get(countState) % 2 === 0,
+  set: ({ set }, newValue) => set(countState, newValue),
+});

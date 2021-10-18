38486b751a045136885871c455db47bbe6cef8e3
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-10-18 23:46:04 +0900

    feat: reset 버튼 추가

--- billboard/src/components/Count/Count.js
+++ billboard/src/components/Count/Count.js
@@ -7,10 +7,11 @@ function Count() {
   const [evenCount, setEvenCount] = useRecoilState(countEvenState);
   const incrementByOne = () => setCount(count + 1);
   const incrementByOneEvenCount = () => setEvenCount(count + 1);
+  const resetCount = useResetRecoilState(countState);
 
-//   useRecoilValue: 값만을 불러올 수 있다. 즉 이 훅은 set 함수를 반환하지 않는다.
-// useSetRecoilState: set 함수만을 불러올 수 있다.
-// useRecoilState: 값, set 함수 두가지 다 불러올 수 있다.
+  //   useRecoilValue: 값만을 불러올 수 있다. 즉 이 훅은 set 함수를 반환하지 않는다.
+  // useSetRecoilState: set 함수만을 불러올 수 있다.
+  // useRecoilState: 값, set 함수 두가지 다 불러올 수 있다.
   return (
     <div>
       Count: {count}
@@ -20,6 +21,7 @@ function Count() {
       Even Count: {evenCount ? '짝수' : '홀수'}
       <br />
       <button onClick={incrementByOneEvenCount}>Even Increment</button>
+      <button onClick={resetCount}>reset</button>
     </div>
   );
 }

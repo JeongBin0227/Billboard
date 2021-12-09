073bf2674849fc538ab47a672d3b41cee23cc792
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-12-09 18:50:47 +0900

    feat: component 배치

--- billboard/src/App.js
+++ billboard/src/App.js
@@ -1,12 +1,56 @@
 import './App.css';
+import styled from 'styled-components';
 
+const Header = styled.div`
+  background-color: red;
+  margin: auto; /* header의 양쪽 여백(margin)을 동등하게 맞춤 -> 수평가운데정렬 */
+  width: 100%;
+  height: 215px;
+  display: flex;
+  align-items: center; /* 하위 요소들 수직 가운데정렬 */
+  position: relative;
+`;
+
+const Main = styled.div`
+  background: darkgray;
+  height: 1000px;
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  grid-gap: 100px;
+`;
+
+const Board = styled.div`
+  background: blue;
+`;
+
+const BoardMain = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 30px;
+  height: 300px;
+`;
+
+const Link = styled.div`
+  background: green;
+`;
 function App() {
   return (
-    <div>
-      <div>hi</div>
-      <div></div>
-      <div></div>
-    </div>
+    <>
+      <Header>
+        <div>hi</div>
+        <div></div>
+        <div></div>
+      </Header>
+      <Main>
+        <Board>
+          <BoardMain>
+            <div style={{ backgroundColor: 'lightgrey' }}>dsad</div>
+            <div style={{ backgroundColor: 'lightgrey' }}>dsad</div>
+          </BoardMain>
+        </Board>
+        <Link></Link>
+      </Main>
+    </>
   );
 }
 

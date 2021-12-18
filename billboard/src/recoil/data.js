9e2de9096181bd7648ea2e52fcb17316bd2018de
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-12-18 16:26:25 +0900

    feat: event api 뼈대 만들기

--- billboard/src/recoil/data.js
+++ billboard/src/recoil/data.js
@@ -5,3 +5,12 @@ const csSelector = selector({
     return await response.json();
   },
 });
+
+const eventsSelector = selector({
+  key: 'eventsSelector',
+  get: async () => {
+    const response = await fetch(`${host}/apis/v1/company/events`);
+    return await response.json();
+  },
+});
+

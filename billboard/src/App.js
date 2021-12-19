71a6a13458dcd7f04e2e242202730f4259df7347
dah2y <95850722+dah2y@users.noreply.github.com>
2021-12-19 15:29:06 +0900

    feat: style 수정 like naver

--- billboard/src/App.js
+++ billboard/src/App.js
@@ -2,53 +2,89 @@ import './App.css';
 import styled from 'styled-components';
 
 const Header = styled.div`
-  background-color: red;
-  margin: auto; /* header의 양쪽 여백(margin)을 동등하게 맞춤 -> 수평가운데정렬 */
+  background-color: ffffff;
+  margin: auto;
   width: 100%;
-  height: 215px;
+  height: 72px;
   display: flex;
+  justify-content: center;
   align-items: center; /* 하위 요소들 수직 가운데정렬 */
   position: relative;
+  box-shadow: 0px 0px 9px rgba(179, 179, 179, 0.7);
 `;
-
+const HeaderContentsWrapper = styled.div`
+  width: 1600px;
+  height: 72px;
+  display: flex;
+  justify-content: space-between;
+  `;
+const HeaderContentsLeft = styled.div`
+  display: flex;
+  margin-left: 40px;
+  margin-top: 24px;
+  margin-down: 24px;
+  `;
+const HeaderContentsRight = styled.div`
+  display: flex;
+  margin: 24px 40px 24px 0;
+  `;
+const Logo = styled.div`
+ 
+  `;
+const Issue = styled.div`
+  padding: 0 24px 0 20px;
+  `;
 const Main = styled.div`
-  background: darkgray;
-  height: 1000px;
+  max-width: 100%;
+  margin: 24px 40px 0 40px;
+  height: auto;
+  display: flex;
+  justify-content: center;
+`;
+const BoardContentsWrapper = styled.div`
+  min-width: 1520px;
   display: grid;
-  grid-template-columns: 2fr 1fr;
-  grid-gap: 100px;
+  grid-gap: 32px;
+  grid-template-columns: 1.54fr 1fr;
 `;
 
-const Board = styled.div`
-  background: blue;
-`;
-
-const BoardMain = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-gap: 30px;
+const BoardLeft = styled.div`
+  background-color: lightgrey;
   height: 300px;
 `;
 
-const Link = styled.div`
-  background: green;
+const BoardRight = styled.div`
+  background-color: lightgrey;
+  height: 300px;
 `;
+
 function App() {
   return (
     <>
       <Header>
-        <div>hi</div>
-        <div></div>
-        <div></div>
+        <HeaderContentsWrapper>
+          <HeaderContentsLeft>
+            <Logo>로고</Logo>
+            <Issue>사내소식</Issue>
+            <div>통계청</div>
+          </HeaderContentsLeft>
+          <HeaderContentsRight>
+            <div>월급날</div>
+            <div>밥시간</div>
+          </HeaderContentsRight>
+        </HeaderContentsWrapper>
       </Header>
       <Main>
-        <Board>
-          <BoardMain>
-            <div style={{ backgroundColor: 'lightgrey' }}>dsad</div>
-            <div style={{ backgroundColor: 'lightgrey' }}>dsad</div>
-          </BoardMain>
-        </Board>
-        <Link></Link>
+          <BoardContentsWrapper>
+            <BoardLeft>
+              dsad
+            </BoardLeft>
+            <BoardRight>
+              fff
+            </BoardRight>
+              
+          </BoardContentsWrapper>
+    
       </Main>
     </>
   );

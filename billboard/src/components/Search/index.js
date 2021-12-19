f38a12fa14f50494c5e82fbd76e2717285a85915
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-12-19 21:54:29 +0900

    feat: Search component 분리

--- billboard/src/components/Search/index.js
+++ billboard/src/components/Search/index.js
@@ -1,5 +1,33 @@
-import React from 'react';
+import styled from 'styled-components';
+import search from '../../../src/img/ico-search-line-big.svg';
 
-export default function Search() {
-  return <div></div>;
-}
+const BoardLeftSearch = styled.div`
+  width: 966px;
+  height: 78px;
+  background-color: #ffffff;
+  box-shadow: 0px 5px 6px rgba(160, 160, 160, 0.11);
+  border-radius: 15px;
+  display: flex;
+`;
+const SearchImg = styled.img`
+  padding: 27px 16px 27px 30px;
+`;
+
+const SearchText = styled.div`
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 23px;
+  color: #bfbfcc;
+  padding-top: 27px;
+`;
+
+const index = () => {
+  return (
+    <BoardLeftSearch>
+      <SearchImg src={search} />
+      <SearchText>구글에서 검색</SearchText>
+    </BoardLeftSearch>
+  );
+};
+
+export default index;

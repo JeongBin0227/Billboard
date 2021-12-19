356a7c6e63a4fdafe47692f16cf72691b62592a5
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-12-19 21:54:23 +0900

    feat: OKR component 분리

---
+++ billboard/src/components/OKR/index.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const BoardLeftOKR = styled.div`
+  width: 967px;
+  height: 212px;
+  background-color: #ffffff;
+  box-shadow: 0px 5px 6px rgba(160, 160, 160, 0.11);
+  border-radius: 15px;
+`;
+
+const index = () => {
+  return <BoardLeftOKR>okr</BoardLeftOKR>;
+};
+
+export default index;

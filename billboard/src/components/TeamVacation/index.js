d3ea3c752cc8ece501d5425b339a29035072dcf5
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-12-19 21:54:49 +0900

    feat: TeamVacation component 분리

---
+++ billboard/src/components/TeamVacation/index.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const BoardRightTeamVacation = styled.div`
+  background-color: #ffffff;
+  box-shadow: 0px 5px 6px rgba(160, 160, 160, 0.11);
+  border-radius: 15px;
+  width: 522px;
+  height: 286px;
+`;
+
+const index = () => {
+  return <BoardRightTeamVacation>팀휴가</BoardRightTeamVacation>;
+};
+
+export default index;

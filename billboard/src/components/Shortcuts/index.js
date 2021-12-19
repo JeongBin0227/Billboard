1854f18fbbebd6b503789ba5f07a2dc9240c9e52
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-12-19 21:54:39 +0900

    feat: Shortcuts component 분리

---
+++ billboard/src/components/Shortcuts/index.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const BoardRightShortcuts = styled.div`
+  background-color: #ffffff;
+  box-shadow: 0px 5px 6px rgba(160, 160, 160, 0.11);
+  border-radius: 15px;
+  width: 521px;
+  height: 398px;
+`;
+
+const index = () => {
+  return <BoardRightShortcuts>바로가기</BoardRightShortcuts>;
+};
+
+export default index;

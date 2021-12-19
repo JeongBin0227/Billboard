35af20e504410daa58da1d474804907e4e9e8f6b
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-12-19 21:54:58 +0900

    feat: Vacation component 분리

---
+++ billboard/src/components/Vacation/index.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const BoardLeftVacation = styled.div`
+  width: 967px;
+  height: 100%;
+  background-color: #ffffff;
+  box-shadow: 0px 5px 6px rgba(160, 160, 160, 0.11);
+  border-radius: 15px;
+`;
+
+const index = () => {
+  return <BoardLeftVacation>휴가</BoardLeftVacation>;
+};
+
+export default index;

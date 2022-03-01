8e79653de8b997a8449736c302b81f474f907a87
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2022-03-01 19:53:38 +0900

    feat: data set 추가

--- billboard/src/recoil/data.js
+++ billboard/src/recoil/data.js
@@ -1,50 +1,50 @@
-import { selector } from 'recoil';
-import { host } from '../static';
+// import { selector } from 'recoil';
+// import { host } from '../static';
 
-const csSelector = selector({
-  key: 'csSelector',
-  get: async () => {
-    const response = await fetch(`${host}/apis/v1/company/cs`);
-    return await response.json();
-  },
-});
+// const csSelector = selector({
+//   key: 'csSelector',
+//   get: async () => {
+//     const response = await fetch(`${host}/apis/v1/company/cs`);
+//     return await response.json();
+//   },
+// });
 
-const eventsSelector = selector({
-  key: 'eventsSelector',
-  get: async () => {
-    const response = await fetch(`${host}/apis/v1/company/events`);
-    return await response.json();
-  },
-});
+// const eventsSelector = selector({
+//   key: 'eventsSelector',
+//   get: async () => {
+//     const response = await fetch(`${host}/apis/v1/company/events`);
+//     return await response.json();
+//   },
+// });
 
-const vacationsSelector = selector({
-  key: 'vacationsSelector',
-  get: async () => {
-    const response = await fetch(`${host}/apis/v1/company/vacations`);
-    return await response.json();
-  },
-});
+// const vacationsSelector = selector({
+//   key: 'vacationsSelector',
+//   get: async () => {
+//     const response = await fetch(`${host}/apis/v1/company/vacations`);
+//     return await response.json();
+//   },
+// });
 
-const healthSelector = selector({
-  key: 'healthSelector',
-  get: async () => {
-    const response = await fetch(`${host}/apis/v1/health`);
-    return await response.json();
-  },
-});
+// const healthSelector = selector({
+//   key: 'healthSelector',
+//   get: async () => {
+//     const response = await fetch(`${host}/apis/v1/health`);
+//     return await response.json();
+//   },
+// });
 
-const weatherSelector = selector({
-  key: 'weatherSelector',
-  get: async () => {
-    const response = await fetch(`${host}/apis/v1/weather`);
-    return await response.json();
-  },
-});
+// const weatherSelector = selector({
+//   key: 'weatherSelector',
+//   get: async () => {
+//     const response = await fetch(`${host}/apis/v1/weather`);
+//     return await response.json();
+//   },
+// });
 
-export {
-  csSelector,
-  eventsSelector,
-  vacationsSelector,
-  healthSelector,
-  weatherSelector,
-};
+// export {
+//   csSelector,
+//   eventsSelector,
+//   vacationsSelector,
+//   healthSelector,
+//   weatherSelector,
+// };
